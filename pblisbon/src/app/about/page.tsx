668c21adc388b33180d32d9dc16fa93bff5b4660
Saba <sabanes048@gmail.com>
2024-06-16 16:25:31 +0100

    vasco

--- pblisbon/src/app/about/page.tsx
+++ pblisbon/src/app/about/page.tsx
@@ -10,12 +10,14 @@ const page = () => {
       <div className='z-10 w-[100px] h-10 bg-[var(--orange)]'></div>
       <h1>Casa de um Espanhol  </h1>
       </div>
+      <a href="/contact">
       <div className='z-10 flex flex-col justify-center items-center '>
       <div className='z-10 trin w-[100px] h-10 bg-[var(--orange)]'></div>
       <div className='z-10 w-[100px] h-10 bg-[var(--orange)]'></div>
       <h1 className='z-10 text-2xl font-bold text-black'>Casa do meu amigo Vasco </h1>
       <img className='w-[50%] z-1  absolute' src="https://images.pexels.com/photos/7283/garden.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
       </div>
+      </a>
       </div>
     </>
   )
--- pblisbon/src/app/contact/page.tsx
+++ pblisbon/src/app/contact/page.tsx
@@ -2,6 +2,9 @@ import Header from '../components/Header';
 
 export default function Page() {
     return (
+      <>
       <Header />
+      <img className='w-screen h-svh' src="https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      </>
     )
   }
