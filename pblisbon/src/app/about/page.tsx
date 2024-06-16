159210707f76ab02fb173a43239b1d1e684acacc
Saba <sabanes048@gmail.com>
2024-06-16 16:09:36 +0100

    Vasco

--- pblisbon/src/app/about/page.tsx
+++ pblisbon/src/app/about/page.tsx
@@ -4,16 +4,17 @@ const page = () => {
   return (
     <>
       <Header />
-      <div className='flex flex-col justify-center items-center gap-10 relative top-10'>
-      <div className='flex flex-col justify-center items-center '>
-      <div className='trin w-[100px] h-10 bg-[var(--orange)]'></div>
-      <div className='w-[100px] h-10 bg-[var(--orange)]'></div>
-      <h1>Casa do Ale </h1>
+      <div className='z-10 flex flex-col justify-center items-center gap-10 relative top-10'>
+      <div className='z-10 flex flex-col justify-center items-center '>
+      <div className='z-10 trin w-[100px] h-10 bg-[var(--orange)]'></div>
+      <div className='z-10 w-[100px] h-10 bg-[var(--orange)]'></div>
+      <h1>Casa de um Espanhol  </h1>
       </div>
-      <div className='flex flex-col justify-center items-center '>
-      <div className='trin w-[100px] h-10 bg-[var(--orange)]'></div>
-      <div className='w-[100px] h-10 bg-[var(--orange)]'></div>
-      <h1>Casa do Vasco </h1>
+      <div className='z-10 flex flex-col justify-center items-center '>
+      <div className='z-10 trin w-[100px] h-10 bg-[var(--orange)]'></div>
+      <div className='z-10 w-[100px] h-10 bg-[var(--orange)]'></div>
+      <h1 className='z-10 text-2xl font-bold text-black'>Casa do meu amigo Vasco </h1>
+      <img className='w-[50%] z-1  absolute' src="https://images.pexels.com/photos/7283/garden.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
       </div>
       </div>
     </>
